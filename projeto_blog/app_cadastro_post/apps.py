2aabe480e004323e1a3c03429d169e48edf8d11b
CaioLopes18 <r9506505@gmail.com>
2024-08-11 18:11:17 -0300

    sim

--- projeto_blog/app_cadastro_post/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppCadastroPostConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "app_cadastro_post"
--- projeto_blog/app_cadastro_post/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class Post(models.Model):
-    id_post = models.AutoField(primary_key=True)
-    titulo = models.CharField(max_length=100)
-    conteudo = models.TextField()
